80a7aa21a9c7b2be13e191056172f6710fcd385d
Danil Efimov <danil_efimov@epam.com>
2018-06-18 23:05:57 +0300

    Added routing + refactoring modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { LogoComponent } from './components/logo/logo.component';
-import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
-import { UserInfoComponent } from './components/user-info/user-info.component';
 import { MaterialsModule } from '../materials/materials.module';
-import { NotFoundComponent } from './components';
+
+import { 
+  BreadcrumbsComponent,
+  HeaderComponent,
+  FooterComponent,
+  LogoComponent,
+  NotFoundComponent,
+  UserInfoComponent,
+ } from './components';
 
 const COMPONENTS = [
   HeaderComponent,
